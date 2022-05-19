847103e67302fd0dcfae8d38d5ce989d474a1fe8
Vadim-Havrylitse <79513261+Vadim-Havrylitse@users.noreply.github.com>
2022-05-19 18:57:00 +0300

    Update 1.sql
    set salary for dev

--- 1.sql
+++ 1.sql
@@ -1,2 +1,16 @@
   ALTER TABLE testgoitdb.developers
   ADD COLUMN salary int;
+  
+  UPDATE testgoitdb.developers
+  SET salary = 12000 
+  WHERE id = 1;
+  
+  UPDATE testgoitdb.developers
+  SET salary = 22000 
+  WHERE id = 2;
+  
+  UPDATE testgoitdb.developers
+  SET salary = 12000 
+  WHERE id = 3;
+  
+  
