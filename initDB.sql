41babdbba9855e76fac6160639803a072ceee74c
Vadim-Havrylitse <79513261+Vadim-Havrylitse@users.noreply.github.com>
2022-05-19 18:51:44 +0300

    Update initDB.sql
    Edit value:
    id int --> id big int 
    enum --> varchar(20)

--- initDB.sql
+++ initDB.sql
@@ -1,23 +1,23 @@
 CREATE DATABASE testgoitdb;
 
 CREATE TABLE testgoitdb.developers (
-	id int AUTO_INCREMENT,
+	id bigint AUTO_INCREMENT,
     name varchar(20) NOT NULL,
     surname varchar(20) NOT NULL,
-    age int CHECK (age>0),
-    sex ENUM('male','female', 'other'),
+    age bigint CHECK (age>0),
+    sex varchar(10),
     PRIMARY KEY(id)
     );
     
 CREATE TABLE testGoITDB.skills (
-	id int AUTO_INCREMENT,
-    industry ENUM('Java', 'C++', 'C#', 'JS'),
-    degree ENUM('Junior', 'Middle', 'Senior') DEFAULT 'Junior',
+	id bigint AUTO_INCREMENT,
+    industry varchar(20),
+    degree varchar(20) DEFAULT 'Junior',
     PRIMARY KEY(id)
     );
 
 CREATE TABLE testgoitdb.projects (
-	id int AUTO_INCREMENT,
+	id bigint AUTO_INCREMENT,
     name varchar(50) UNIQUE,
     budget bigint NOT NULL, 
     release_date date,
@@ -25,7 +25,7 @@ CREATE TABLE testgoitdb.projects (
     );
     
 CREATE TABLE testgoitdb.companies (
-	id int AUTO_INCREMENT,
+	id bigint AUTO_INCREMENT,
     name varchar(50) NOT NULL,
     country varchar(20),
     city varchar(20),
@@ -33,33 +33,33 @@ CREATE TABLE testgoitdb.companies (
     );
     
 CREATE TABLE testgoitdb.customers (
-	id int AUTO_INCREMENT,
+	id bigint AUTO_INCREMENT,
     name varchar(20),
     surname varchar(20),
     PRIMARY KEY(id)
     );
 
 CREATE TABLE testgoitdb.developers_projects (
-	developers_id int,
-    projects_id int,
+	developers_id bigint,
+    projects_id bigint,
     FOREIGN KEY (projects_id) REFERENCES testgoitdb.projects(id),
     FOREIGN KEY (developers_id) REFERENCES testgoitdb.developers(id)
     );
     
 CREATE TABLE testgoitdb.developers_skills (
-	developers_id int,
-    skills_id int,
+	developers_id bigint,
+    skills_id bigint,
     FOREIGN KEY (skills_id) REFERENCES testgoitdb.skills(id),
     FOREIGN KEY (developers_id) REFERENCES testgoitdb.developers(id)
     );
     
 ALTER TABLE testgoitdb.projects
-	ADD COLUMN companies_id int NOT NULL,
-    ADD COLUMN customers_id int NOT NULL,
+	ADD COLUMN companies_id bigint NOT NULL,
+    ADD COLUMN customers_id bigint NOT NULL,
     ADD FOREIGN KEY (customers_id) REFERENCES testgoitdb.customers(id),
     ADD FOREIGN KEY (companies_id) REFERENCES testgoitdb.companies(id);
     
 ALTER TABLE testgoitdb.developers
-	ADD COLUMN companies_id int,
+	ADD COLUMN companies_id bigint,
     ADD FOREIGN KEY (companies_id) REFERENCES testgoitdb.companies(id);
 
