6e7e4ffadecb04ad1edb7b1321b838eb6ed5461e
Vadim-Havrylitse <79513261+Vadim-Havrylitse@users.noreply.github.com>
2022-05-22 20:20:35 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,2 +1,14 @@
 ALTER TABLE projects
 ADD COLUMN cost bigint not NULL;
+
+UPDATE projects 
+SET cost = 12500
+WHERE id = 2;
+
+UPDATE projects 
+SET cost = 230000
+WHERE id = 1;
+
+UPDATE projects 
+SET cost = 12500
+WHERE id = 3;
