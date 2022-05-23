bd5f20f65cba4c3b788cf17ba96fa80c2748d7b9
Vadim-Havrylitse <79513261+Vadim-Havrylitse@users.noreply.github.com>
2022-05-23 09:58:36 +0300

    Update 2.sql
    Maked sql reqwest more simple

--- 2.sql
+++ 2.sql
@@ -1,20 +1,9 @@
-SELECT testgoitdb.projects.name AS projects_with_maxsalary_of_own_developers
-FROM testgoitdb.projects
-WHERE id IN (
-	SELECT projects_id
-	FROM (
-		SELECT tab1.projects_id, SUM(salary) AS sumsalary
-		FROM testgoitdb.developers_projects AS tab1
-		LEFT JOIN testgoitdb.developers AS tab2
-			ON (tab1.developers_id = tab2.id)
-		GROUP BY tab1.projects_id) AS temptab1
-	INNER JOIN (
-		SELECT SUM(salary) AS sumsalary
-		FROM testgoitdb.developers_projects AS tab1
-		LEFT JOIN testgoitdb.developers AS tab2
-			ON (tab1.developers_id = tab2.id)
-		GROUP BY tab1.projects_id
-		ORDER BY SUM(salary) DESC
-		LIMIT 1) AS temptab2
-	ON temptab1.sumsalary = temptab2.sumsalary
-    );
+SELECT projects.name, SUM(salary)
+FROM projects
+LEFT JOIN developers_projects
+	ON projects.id = developers_projects.projects_id
+LEFT JOIN developers
+	ON developers.id = developers_projects.developers_id
+GROUP BY projects.id
+ORDER BY SUM(salary) DESC
+LIMIT 1;
